64abfa4d723202717348fcc0c361f58addb5afc9
A9219 <alexey1978malyshev@gmail.com>
2023-04-22 00:29:01 +0300

    catalog info method added

---
+++ lec7 - recurse-continue/Program.cs
@@ -0,0 +1,23 @@
+﻿void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    //получение информации  о файлах в папке
+
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+
+string path = "C:/Users/A9219/Documents/geek brains/C#";
+CatalogInfo(path);
